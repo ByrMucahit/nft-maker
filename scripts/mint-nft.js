52a7b8156a7e83293e23205255486f8822da4137
mucahit bayar <mucahit.bayar@mavidev.com>
2022-10-23 20:37:30 +0300

    Removed address

--- scripts/mint-nft.js
+++ scripts/mint-nft.js
@@ -13,18 +13,18 @@ console.log(JSON.stringify(contract.abi));
 
 // Create a signer
 const privateKey = process.env.PRIVATE_KEY;
-const signer = new ethers.Wallet("dc66ade0ba736acbb794cc5165c97394ea27e86386665ca6df434986d1e1bb35", provider);
+const signer = new ethers.Wallet("{Your WAllet address}", provider);
 
 // Get contract ABI and address
 const abi = contract.abi;
-const contractAddress = '0x530cd7bD9755149F9C45ADC5790AdeA48C9B6240'
+const contractAddress = '{your_contract_adree}'
 
 
 // Create a contract instance
 const myNftContract = new ethers.Contract(contractAddress, abi, signer);
 
 // Get the NFT Metadata IPES URL
-const tokenUri = "https://gateway.pinata.cloud/ipfs/Qmeg5bHjeVK6TWdiwSVr4ZXrk4WgDwB9qi3EJp6g5g2YTz";
+const tokenUri = "https://gateway.pinata.cloud/ipfs/{your_address}";
 
 // Call mintNFT function
 const mintNFT = async () => {
